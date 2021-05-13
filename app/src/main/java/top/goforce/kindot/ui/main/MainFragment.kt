cbf0358bcdd7b1fdd8ab06e163055dab331e5106
GoFly233 <gofly233@163.com>
2021-05-14 00:13:27 +0800

    :sparkles: 将 MainFragment 的父类改为 BaseFragment

--- app/src/main/java/top/goforce/kindot/ui/main/MainFragment.kt
+++ app/src/main/java/top/goforce/kindot/ui/main/MainFragment.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import top.goforce.kindot.R
+import top.goforce.kindot.base.BaseFragment
 
-class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
+class MainFragment : BaseFragment() {
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
